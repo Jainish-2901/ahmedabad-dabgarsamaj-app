1347461149957d91b07aafee2b33d5ef5fccf5f9
Jainish Dabgar <jainishdabgar2901@gmail.com>
2026-09-04 11:22:34 +0530

    fix(android): use Google Cloud Maven Central mirror to prevent 429 rate limit

--- plugins/withMavenMirrors.js
+++ plugins/withMavenMirrors.js
@@ -3,8 +3,8 @@ const { withProjectBuildGradle } = require('@expo/config-plugins');
 module.exports = function withMavenMirrors(config) {
   return withProjectBuildGradle(config, (config) => {
     let contents = config.modResults.contents;
-    const mirrors = `maven { url 'https://repo1.maven.org/maven2/' }\n    maven { url 'https://maven.aliyun.com/repository/public' }\n    mavenCentral()`;
-    if (contents && !contents.includes('repo1.maven.org')) {
+    const mirrors = `maven { url 'https://maven-central.storage-download.googleapis.com/maven2/' }\n    maven { url 'https://maven.aliyun.com/repository/public' }\n    mavenCentral()`;
+    if (contents && !contents.includes('maven-central.storage-download.googleapis.com')) {
       contents = contents.replace(/mavenCentral\(\)/g, mirrors);
       config.modResults.contents = contents;
     }
